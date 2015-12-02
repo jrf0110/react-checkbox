1739d07bc0504d18dfa470743cb7d37edf88e54f
John Fawcett <jrf0110@gmail.com>
2015-12-01 18:03:52 -0600

    initial

---
+++ index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default React.createClass({
+  getDefaultProps: function(){
+    return {
+      onChange: function( checked, value ){}
+    }
+  }
+
+, render: function(){
+    return (
+      <div className="checkbox-wrapper">
+        {this.props.children}
+        <input id={this.props.id}
+          type="checkbox"
+          ref="inputEl"
+          checked={this.props.checked}
+          value={this.props.value}
+          readOnly
+        />
+        <div
+          contentEditable="true"
+          className="checkbox-facade"
+          onClick={this.onClick}
+          onKeyPress={this.onKeyPress}
+          onKeyUp={this.onWrapperText}></div>
+      </div>
+    );
+  }
+
+, getInputEl: function(){
+    return ReactDOM.findDOMNode( this.refs.inputEl );
+  }
+
+, toggle: function(){
+    return !this.getInputEl().checked;
+  }
+
+, onClick: function( e ){
+    this.props.onChange( this.toggle(), this.getInputEl().value );
+  }
+
+  // Prevent text from showing at all
+, onKeyPress: function( e ){
+    e.preventDefault();
+    e.target.textContent = '';
+  }
+
+, onWrapperText: function( e ){
+    if ( e.which === 32 ){
+      this.props.onChange( this.toggle(), this.getInputEl().value );
+    }
+  }
+});
